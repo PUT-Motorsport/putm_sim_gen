ffc10eafde4865c5ee6a404e1727c3b4fc8b9a02
KozAAAAA <mateusz-koza@outlook.com>
2024-07-23 15:04:30 +0200

    Change default acc pedal value from 1 to 0

--- read.c
+++ read.c
@@ -5,7 +5,7 @@ void read_inputs(){
   tv_P.WheelSp_RL_Value = 0;
   tv_P.WheelSp_RR_Value = 0;
   tv_P.delta_Value = 0;
-  tv_P.acc_pedal_Value = 1; 
+  tv_P.acc_pedal_Value = 0; 
   tv_P.brake_pedal_Value = 0;
   tv_P.ax_Value = 0;
   tv_P.ay_Value = 0;
