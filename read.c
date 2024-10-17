581e2a59ac29bdafbfff0be171de687cccc41fb3
Filip Michalski <filip.michalski.1@student.put.poznan.pl>
2024-10-17 20:32:46 +0200

    change_read_acc_pedal_to_0

--- read.c
+++ read.c
@@ -5,7 +5,7 @@ void read_inputs(){
   tv_code_P.whl_speed_rl_Value = 0;
   tv_code_P.whl_speed_rr_Value = 0;
   tv_code_P.delta_Value = 0;
-  tv_code_P.acc_pedal_Value = 1; 
+  tv_code_P.acc_pedal_Value = 0; 
   tv_code_P.ax_Value = 0;
   tv_code_P.ay_Value = 0;
   tv_code_P.yaw_rate_Value = 0;
